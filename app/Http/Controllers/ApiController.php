8b28006df541fa4a13034df6fc6d9ba9593c2ba7
Vinay Singh <codewithvinaysingh@gmail.com>
2022-01-01 09:41:34 +0530

    Update API from Query Builder to Eloquent ORM

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -13,21 +14,13 @@ class ApiController extends Controller
     public function post($id = null)
     {
         if ($id) {
-            $data['post'] = Post::select('id', 'thumbnail', 'title', 'description')->find($id);
-            $data['comments'] = DB::table('comments')
-                ->join('posts', 'comments.post_id', '=', 'posts.id')
-                ->where('posts.id', '=', $id)
-                ->select('comments.comment')
-                ->latest('comments.created_at')
-                ->get();
-            return $data;
+            return Post::with('category', 'comments')
+                ->where('posts.id', '=', $id)->first();
         } else {
 
-            return DB::table('posts')
-                ->join('categories', 'posts.category_id', '=', 'categories.id')
-                ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-                ->select('posts.id', 'posts.thumbnail', 'posts.title', 'posts.description', 'posts.created_at', 'categories.name as category', DB::raw('count(comments.id) as total_comment'))
-                ->groupBy('posts.id')
+            //     ->select('posts.id', 'posts.thumbnail', 'posts.title', 'posts.description', 'posts.created_at', 'categories.name as category', DB::raw('count(comments.id) as total_comment'))
+
+            return Post::with('category', 'comments')
                 ->latest('posts.created_at')
                 ->paginate(5);
         }
@@ -35,41 +28,28 @@ class ApiController extends Controller
     public function category($id = null)
     {
         if ($id) {
-            return  DB::table('posts')
-                ->join('categories', 'posts.category_id', '=', 'categories.id')
-                ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-                ->where('posts.category_id', '=', $id)
-                ->select('posts.id', 'posts.thumbnail', 'posts.title', 'posts.description', 'posts.created_at', 'categories.name as category', DB::raw('count(comments.id) as total_comment'))
-                ->groupBy('posts.id')
-                ->limit(10)
-                ->latest('posts.created_at')
-                ->paginate(5);
+           
+            //     ->select('posts.id', 'posts.thumbnail', 'posts.title', 'posts.description', 'posts.created_at', 'categories.name as category', DB::raw('count(comments.id) as total_comment'))
+
+            return Post::with('category', 'comments')
+            ->where('posts.category_id', '=', $id)
+            ->paginate(5);
         } else {
-            return DB::table('categories')
-                ->leftJoin('posts', 'categories.id', '=', 'posts.category_id')
-                ->select('categories.id', 'categories.name', DB::raw('count(posts.id) as total_post'))
-                ->groupBy('categories.id')
-                ->get();
+            return Category::all();
         }
     }
 
     public function search($s)
     {
-            $posts = DB::table('posts')
-                ->join('categories', 'posts.category_id', '=', 'categories.id')
-                ->leftJoin('comments', 'posts.id', '=', 'comments.post_id')
-                ->where('posts.title', 'LIKE', '%' . $s . '%')
-                // ->where('posts.description', 'LIKE', '%' . $s . '%')
-                ->select('posts.id', 'posts.thumbnail', 'posts.title', 'posts.description', 'posts.created_at', 'categories.name as category', DB::raw('count(comments.id) as total_comment'))
-                ->groupBy('posts.id')
-                ->latest('posts.created_at')
-                ->paginate(5);
-                if(count($posts->items())){
-                    return $posts;
-                }else{
-                    return ['result'=>'no result found'];
-                }
-            
+        $posts = Post::with('category', 'comments')
+            ->where('posts.title', 'LIKE', '%' . $s . '%')
+            ->latest('posts.created_at')
+            ->paginate(5);
+        if ($posts->count()) {
+            return $posts;
+        } else {
+            return ['result' => 'no result found'];
+        }
     }
 
     public function add_comment(Request $request)
